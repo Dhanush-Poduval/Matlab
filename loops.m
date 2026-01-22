a9a255b840e395651776f4ac553abb22b5f6a176
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-01-22 16:45:49 +0530

    Fix. made the graph axes right

--- loops.m
+++ loops.m
@@ -5,7 +5,9 @@ fibonnaci_array=[1,2,3,4,5,6,7,8,9,10];
 for n =3:N
     f(n)=f(n-1)+f(n-2);
 end
+plot(fibonnaci_array,f(1:10));
 xlabel('values');
 ylabel('Fibbonaci numbers');
-plot(f(1:10),fibonnaci_array);
+title("Fibonnaci Sequence");
+
  
