d1159e34620afe3347a4e5adddcb3465a6e01425
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-01-23 14:10:34 +0530

    feat:checked the orientation of the tool0 body

--- randomconfig.m
+++ randomconfig.m
@@ -1,5 +1,7 @@
 robot=loadrobot("fanucLRMate200ib");
 showdetails(robot);
 config=homeConfiguration(robot);
+tform =getTransform(robot,config,'tool0','base');
 disp(config);
+disp(tform);
 show(robot);
