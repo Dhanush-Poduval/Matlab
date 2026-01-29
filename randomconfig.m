afd65aabbcaa0ff01e80ce29ffcf0b569f161a3a
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-01-29 13:10:48 +0530

    feat:turned the diffrent joints

--- randomconfig.m
+++ randomconfig.m
@@ -2,6 +2,7 @@ robot=loadrobot("fanucLRMate200ib");
 showdetails(robot);
 config=homeConfiguration(robot);
 tform =getTransform(robot,config,'tool0','base');
+config(3).JointPosition=pi/2;
 disp(config);
 disp(tform);
-show(robot);
+show(robot,config);
