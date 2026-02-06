bc4677e2418729fe5aa9ca023a2582ee8e1870da
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-07 03:48:18 +0530

    feat:made the function to check posiiton and it point

--- my_robot.m
+++ my_robot.m
@@ -5,14 +5,14 @@ body1=rigidBody('Body1');
 jnt1=rigidBodyJoint('jnt1','revolute');
 setFixedTransform(jnt1,trvec2tform([0 0 0]));
 jnt1.JointAxis=[0 0 1];
-addVisual(body1,'Cylinder',[0.03 0.3]);
+%addVisual(body1,'Cylinder',[0.03 0.3]);
 body1.Joint=jnt1;
 robot=rigidBodyTree;
 addBody(robot,body1,'base');
 body2=rigidBody('body2');
 jnt2=rigidBodyJoint('jnt2','revolute');
 setFixedTransform(jnt2,trvec2tform([L1 0 0]))
-addVisual(body2,'Cylinder',[0.03 0.3])
+%addVisual(body2,'Cylinder',[0.03 0.3])
 body2.Joint=jnt2;
 jnt2.JointAxis=[0 0 1];
 addBody(robot,body2,'Body1');
@@ -20,7 +20,7 @@ body3=rigidBody('tool_tip');
 jnt3=rigidBodyJoint('jnt3','fixed');
 setFixedTransform(jnt3,trvec2tform([L2+EE 0 0]));
 %jnt3.JointAxis=[0 0 1];
-addVisual(body3,'Cylinder',[0.03 0.3]);
+%addVisual(body3,'Cylinder',[0.03 0.3]);
 body3.Joint=jnt3;
 q1=pi/4;
 q2=pi/6;
@@ -42,8 +42,8 @@ disp([x_a ,y_a]);
 disp([x_m ,y_m]);
 pos=tform2trvec(t);
 disp(pos);
-s=show(robot ,config);
-hold(s,'on');
+%s=show(robot ,config);
+%hold(s,'on');
 
 %circle formation for later ik
 center_x=0.3;
@@ -55,9 +55,9 @@ y=center_y+r*sin(theta);
 points=[x;y;zeros(size(x))];
 r_points=points';
 disp(size(r_points));
-plot(x,y,'b','LineWidth',2)
-axis(s,'equal')
-grid (s,'on')
+%plot(x,y,'b','LineWidth',2)
+%axis(s,'equal')
+%grid (s,'on')
 %the ik part using normal inverseKinematics function as dont need multiple
 %constraints
 q0=homeConfiguration(robot);
@@ -69,7 +69,18 @@ for i=1:10
     point=r_points(i,:);
     
     [a,b]=ik(endEffector,trvec2tform(point),weights,q_initial);
-    %ik_arr(i,:)=ik_var;
+    ik_arr(i,:)=a;
     q_initial=ik_var;
 end
 disp(b);
+
+%for like animating the movement
+figure
+show(robot,ik_arr(210,:))
+view(2);
+ax=gca;
+ax.Projection='orthographic';
+hold on;
+plot(r_points(:,1),r_points(:,2));
+%axis([-0.1 0.7 -0.3 0.5 -0.2 0.2])
+
