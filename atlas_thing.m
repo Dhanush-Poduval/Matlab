d28c681e85adacddc9cde6ca43a44ec431b6a1fb
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-09 13:20:37 +0530

    feat:started to create a cuble with atlas

--- atlas_thing.m
+++ atlas_thing.m
@@ -3,4 +3,12 @@ showdetails(atlas);
 config=homeConfiguration(atlas);
 config(7).JointPosition=pi/2;
 disp(config);
+endEffector="l_hand_force_torque";
+ik=inverseKinematics("RigidBodyTree",atlas);
+%creation of the cube
+side=0.5;
+volume=side*side*side;
+sa=6*side*side;
+
+
 show(atlas,config);
