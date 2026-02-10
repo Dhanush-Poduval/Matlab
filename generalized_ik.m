dcfda3b155ca43fa37ba45f5fcfd1aa89fee6801
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-10 22:32:26 +0530

    Fix.reveretd back to original data row format

--- generalized_ik.m
+++ generalized_ik.m
@@ -1,4 +1,4 @@
-robot=loadrobot("kukaIiwa14");
+robot=loadrobot("kukaIiwa14","DataFormat","row");
 showdetails(robot);
 endEffector="iiwa_link_ee_kuka";
 cup_Height=0.2;
@@ -18,29 +18,22 @@ vector5=q0.vector5;
 vector6=q0.vector6;
 vector7=q0.vector7;
 %}
-woah=struct2cell(q0);
-out=cat(2,woah{:});
-disp(out);
 %vector_q0=[vector1 vector2 vector3 vector4 vector5 vector6 vector7];
 %disp(vector_q0);
 ik=inverseKinematics("RigidBodyTree",robot);
 weights=[0 0 0 1 1 1];
 [a,~]=ik(endEffector,trvec2tform(cup_position),weights,q0);
-disp(a);
-holy=struct2cell(a);
-a_out=cat(2,holy{:});
-disp(holy);
 t = linspace(0, 1, 50);
-qTrajectory = q0 + t' * (a - q0); 
+qTrajectory = q0 + t' * (a-q0); 
 overall_frames=size(qTrajectory,1);
-disp(a);
 figure
 hold on;
 axis equal;
+view(3);
 surf(x,y,z);
 for i=1:overall_frames
     show(robot,qTrajectory(i,:),"PreservePlot",false,"FastUpdate",true);
     plot(qTrajectory(i,:),'Color','r','LineStyle','--');
-    disp(qTrajectory(i,:));
+    %disp(qTrajectory(i,:));
     drawnow;
 end
