7802fc8fc0b63209c99f3855cf586b301d9d61f2
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-10 18:04:39 +0530

    feat:made the generalized ik function of the robot and animated it

--- generalized_ik.m
+++ generalized_ik.m
@@ -9,8 +9,24 @@ cup_position=[-0.5,0.6,cup_Height/2];
 x=x-0.5;
 y=y+0.6;
 z=z*cup_Height;
+q0=homeConfiguration(robot);
+
+
+ik=inverseKinematics("RigidBodyTree",robot);
+weights=[1 1 1 1 1 1];
+[a,~]=ik(endEffector,trvec2tform(cup_position),weights,q0);
+disp(a);
+
+t = linspace(0, 1, 50);
+qTrajectory = q0 + t' * (a - q0); 
+overall_frames=size(qTrajectory,1);
+
+figure
 hold on;
-surf(x,y,z);
-show(robot);
 axis equal;
-hold off
+surf(x,y,z);
+for i=1:overall_frames
+    show(robot,qTrajectory(i,:),"PreservePlot",false,"FastUpdate",true);
+    disp(qTrajectory(i,:));
+    drawnow;
+end
