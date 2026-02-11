3e84df5a497dbfac1e09faf6d373adec68cbec80
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-11 11:10:56 +0530

    feat:made the generated cpp code for generalisedik

--- path_taken_ik.m
+++ path_taken_ik.m
@@ -1,4 +1,9 @@
-function [x,y]=path_taken_ik(q_inital,endEffector,final_pos)
+function [x,y]=path_taken_ik(q_inital,endEffector,final_pos)%#codegen
+arguments
+    q_inital (1,:) double
+    endEffector (1,64) char
+    final_pos  (1,:) double
+end
     b=generalizedRobotforcodeGen();
     weights=[1 1 1 1 1 1];
     ik=inverseKinematics("RigidBodyTree",b);
