178232b543f7e70c37cf1c7eedb66bd290e0281b
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-27 13:25:07 +0530

    Fix.made the arm orientatiomn right but link rotation is wrong now

--- loading_urdf.m
+++ loading_urdf.m
@@ -15,14 +15,14 @@ check_base=getTransform(robot,config,"base_link");
 disp(check_base);
 q0=config;
 ik=inverseKinematics("RigidBodyTree",robot);
-endEffector="claw";
+endEffector="pulley";
 
 weights=[1 1 1 1 1 1];
 [a,~]=ik(endEffector,trvec2tform(final_position),weights,q0);
 disp(a);
 
 disp(config);
-%{
+
 getTransform(robot,homeConfiguration(robot),"turntable","base_link")
 figure;
 ax=gca;
@@ -34,6 +34,8 @@ overall_frames=size(qTrajectory,1);
 surf(x,y,z);
 grid on;
 view(3);
+
+
 for i=1:overall_frames
    
    
@@ -42,24 +44,27 @@ for i=1:overall_frames
        
         current_eepos=getTransform(robot,qTrajectory(i,:),"turntable");
         current_ee=current_eepos(1:3,4);
+        %{
         current_eepos2=getTransform(robot,qTrajectory(i,:),"roll");
         current_ee2=current_eepos2(1:3,4);
         current_eepos3=getTransform(robot,qTrajectory(i,:),"worm_pitch");
         current_ee3=current_eepos3(1:3,4);
         current_eepos4=getTransform(robot,qTrajectory(i,:),"pitch");
         current_ee4=current_eepos4(1:3,4);
+        %}
         scatter3(current_ee(1),current_ee(2),current_ee(3),'filled','y');
+        %{
         scatter3(current_ee2(1),current_ee2(2),current_ee2(3),'filled','r');
         scatter3(current_ee3(1),current_ee3(2),current_ee3(3),'black');
         scatter3(current_ee4(1),current_ee4(2),current_ee4(3),'green');
-       
+        %}
         disp(current_ee);
         %disp(qTrajectory(i,:));
         drawnow;
     
 end
-%}
-show(robot, a, "Frames","off", "PreservePlot", false);
+
+%show(robot, a, "Frames","off", "PreservePlot", false);
 %show(robot,a,"PreservePlot",true);
 camlight;
 lighting gouraud;
