8feb1f015acaa8e4fbaf5801abd35471385fd501
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-05 18:57:15 +0530

    feat:added joystick sim gotta test

--- rmrc/simulation_rmrc.m
+++ rmrc/simulation_rmrc.m
@@ -9,5 +9,17 @@ eul=rotm2eul(rotation);
 inital_ee_pos=check_base(1:3,4);
 disp(inital_ee_pos);
 disp(check_base);
-show(robot,config);
-showdetails(robot);
+%joystick sim
+
+joy=sim3d.io.Joystick();
+while true 
+    axes=axis(joy);
+    buttons=button(joy);
+    x=axes(1);
+    y=axes(2);
+    disp([x,y]);
+    pause(0.01);
+end
+
+%show(robot,config);
+%showdetails(robot);
