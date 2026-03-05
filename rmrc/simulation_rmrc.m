f612779198158bdf58bbd7f5cc1cd9d3d2cc03d4
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-05 18:23:01 +0530

    feat:got the transform for the base joint

--- rmrc/simulation_rmrc.m
+++ rmrc/simulation_rmrc.m
@@ -1,2 +1,7 @@
 robot=loadrobot("kukaIiwa14","DataFormat","row");
-show(robot);
+config=randomConfiguration(robot);
+check_base=getTransform(robot,config,"world");
+endEffector="iiwa_link_ee_kuka";
+disp(check_base);
+show(robot,config);
+showdetails(robot);
