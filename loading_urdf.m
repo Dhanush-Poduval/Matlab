af402741dcbeb0a32bbcf19a23298b8e88099b50
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-20 14:19:30 +0530

    feat:tested the urdf for the new arm

--- loading_urdf.m
+++ loading_urdf.m
@@ -1,4 +1,4 @@
-robot=importrobot("./rudra_arm/urdf/rudra_arm.urdf");
+robot=importrobot("./rudra_arm/urdf/N1.urdf");
 robot.DataFormat="row";
 showdetails(robot);
 disp(robot.BaseName);
@@ -9,7 +9,7 @@ scatter3(final_position(1),final_position(2),final_position(3),'yellow');
 x=x+1.6;
 y=y+0.4;
 z=z*0.4;
-config = homeConfiguration(robot);
+config = randomConfiguration(robot);
 
 check_base=getTransform(robot,config,"base_link");
 disp(check_base);
